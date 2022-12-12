940a96e781382af3ef5f7a1345c192a592aa7cb2
Mahmoud Shehab <mahmoud.x.mostafa@gmail.com>
2022-12-12 21:52:24 +0200

    update main menu & add main functionality: createDB, listDBs and dropDB

--- DBMS.sh
+++ DBMS.sh
@@ -1,22 +1,55 @@
 #!/bin/bash
-# mkdir DBMS 2>> /dev/null
+mkdir DBMS 2>> /dev/null
 function mainMenu {
     ch=$(zenity --list \
-    --title="Main Menu:" \
-    --column="Bug Number" \
+    --title="Main Menu" \
+    --column="Operations" \
     "Create DB" \
     "List DBs" \
     "Select DB" \
     "Drop DB" \
-    "Exit")
-    echo $ch
+    "Exit");
     case $ch in
-        "Create DB") echo "Create DB";;
-        "List DBs") echo "List DBs";;
+        "Create DB") createDB;;
+        "List DBs") listDBs;;
         "Select DB") echo "Select DB";;
-        "Drop DB") echo "Drop DB";;
+        "Drop DB") dropDB;;
         "Exit") exit;;
-        *) zenity --error --title="Wrong Choice" --text="Please Choose Again"; mainMenu;
+        *) zenity --error --title="Error Message" --text="Please Choose Again"; mainMenu;;
     esac
 }
 mainMenu
+function createDB {
+    dbName=$(zenity --entry --title="Dtabase Name" --text="Enter Database Name");
+    mkdir ./DBMS/$dbName 2>> /dev/null;
+    if [[ $? == 0 ]]
+    then
+        zenity --info --title="Info Message" --text="Database Created Successfully";
+    else
+        zenity --error --title="Error Message" --text="Error Creating Database $dbName";
+    fi
+    mainMenu;
+}
+function listDBs {
+    list=$(ls -Al ./DBMS/ | grep ^d | wc -l);
+    if [[ $list == 0 ]]
+    then
+        zenity --info --title="Info Message" --text="No Databases To Be Listed";
+    else
+        zenity --list --title="List of Databases" --column="Database" $(ls -Al ./DBMS/ | cut -d" " -f9);
+    fi
+    mainMenu;
+}
+function dropDB {
+    dbName=$(zenity --list --title="List of Databases" --column="Database" $(ls -Al ./DBMS/ | cut -d" " -f9));
+    if [[ $dbName != "" ]]
+    then
+        rm -r ./DBMS/$dbName 2>> /dev/null;
+        if [[ $? == 0 ]]; then
+            zenity --info --title="Info Message" --text="Database Dropped Successfully";
+        else
+            zenity --error --title="Error Message" --text="Database Not found";
+        fi
+    fi
+    mainMenu;
+}
