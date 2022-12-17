58547a6bdd5185ba87729ca9a26b0399d3c6ec84
Mahmoud Shehab <mahmoud.x.mostafa@gmail.com>
2022-12-18 01:58:31 +0200

    updates & add insert into table + delete from table functionalities

--- DBMS.sh
+++ DBMS.sh
@@ -48,8 +48,6 @@ function dropDB {
         if [[ $? == 0 ]]
         then
             zenity --info --title="Info Message" --text="Database Dropped Successfully";
-        else
-            zenity --error --title="Error Message" --text="Database Not found";
         fi
     fi
     mainMenu;
@@ -86,9 +84,9 @@ function tableMenu {
         "Create Table") createTable;;
         "List Tables") listTables;;
         "Drop Table") dropTable;;
-        "Insert Into Table") echo "Insert Into Table";;
+        "Insert Into Table") insert;;
         "Select From Table") echo "Select From Table";;
-        "Delete From Table") echo "Delete From Table";;
+        "Delete From Table") deleteFromTable;;
         "Update Table") echo "Update Table";;
         "Back To Main Menu") cd ../.. 2>> /dev/null; mainMenu;;
         "Exit") exit;;
@@ -103,11 +101,11 @@ function createTable {
         tableMenu;
     fi
     colsNum=$(zenity --entry --title="Number of Columns" --text="Enter Number of Columns");
-    counter=1
-    sep=":"
-    rSep="\n"
-    pKey=""
-    metaData="Field"$sep"Type"$sep"key"
+    counter=1;
+    sep=":";
+    rSep="\n";
+    pKey="";
+    metaData="Field"$sep"Type"$sep"key";
     while [[ $counter -le $colsNum ]]
     do
         colName=$(zenity --entry --title="Name of Column No.$counter" --text="Enter Name of Column No.$counter");
@@ -142,11 +140,11 @@ function createTable {
         else
             temp=$temp$colName$sep;
         fi
-        ((counter++))
+        ((counter++));
     done
-    touch $tableName .$tableName
-    echo -e $metaData  >> .$tableName
-    echo -e $temp >> $tableName
+    touch $tableName .$tableName;
+    echo -e $metaData  >> .$tableName;
+    echo -e $temp >> $tableName;
     if [[ $? == 0 ]]
     then
         zenity --info --title="Info Message" --text="Table Created Successfully";
@@ -173,8 +171,98 @@ function dropTable {
         rm $tableName .$tableName 2>> /dev/null;
         if [[ $? == 0 ]]; then
             zenity --info --title="Info Message" --text="Table Dropped Successfully";
+        fi
+    fi
+    tableMenu;
+}
+function insert {
+    tableName=$(zenity --list --title="List of Tables" --column="Table" $(ls -l | cut -d" " -f9));
+    if [[ $tableName != "" ]]
+    then
+        colsNum=$(awk 'END{print NR}' .$tableName);
+        sep=":";
+        rSep="\n";
+        for (( i = 2; i <= $colsNum; i++ ))
+        do
+            colName=$(awk 'BEGIN{FS=":"}{ if(NR=='$i') print $1}' .$tableName);
+            colType=$(awk 'BEGIN{FS=":"}{if(NR=='$i') print $2}' .$tableName);
+            colKey=$(awk 'BEGIN{FS=":"}{if(NR=='$i') print $3}' .$tableName);
+            data=$(zenity --entry --title="Column: $colName" --text="Enter Value of Type $colType");
+            if [[ $colType == "int" ]]
+            then
+                while ! [[ $data =~ ^[0-9]*$ ]]
+                do
+                    zenity --error --title="Error Message" --text="Invalid Data Type";
+                    data=$(zenity --entry --title="Column: $colName" --text="Enter Value of Type $colType");
+                done
+            fi
+            if [[ $colKey == "PK" ]]
+            then
+                while [[ true ]]
+                do
+                    if [[ $data =~ ^[$(awk 'BEGIN{FS=":"; ORS=" "}{if (NR != 1) print $(('$i'-1))}' $tableName 2>> /dev/null)]$ ]]
+                    then
+                        zenity --error --title="Error Message" --text="Invalid Input for PK";
+                        data=$(zenity --entry --title="Column: $colName" --text="Enter Value of Type $colType");
+                    else
+                        break;
+                    fi
+                done
+            fi
+            if [[ $i == $colsNum ]]
+            then
+                row=$row$data$rSep;
+            else
+                row=$row$data$sep;
+            fi
+        done
+        echo -e $row"\c" >> $tableName;
+        if [[ $? == 0 ]]
+        then
+            zenity --info --title="Info Message" --text="Data Inserted Successfully";
         else
-            zenity --error --title="Error Message" --text="Table Not found";
+            zenity --error --title="Error Message" --text="Error Inserting Data into Table $tableName";
+        fi
+        row="";
+    fi
+    tableMenu;
+}
+function deleteFromTable {
+    tableName=$(zenity --list --title="List of Tables" --column="Table" $(ls -l | cut -d" " -f9));
+    if [[ $tableName != "" ]]
+    then
+        colsNum=$(awk 'END{print NR}' .$tableName);
+        sep=":";
+        rSep="\n";
+        for (( i = 2; i <= $colsNum; i++ ))
+        do
+            colName=$(awk 'BEGIN{FS=":"}{ if(NR=='$i') print $1}' .$tableName);
+            colType=$(awk 'BEGIN{FS=":"}{if(NR=='$i') print $2}' .$tableName);
+            colKey=$(awk 'BEGIN{FS=":"}{if(NR=='$i') print $3}' .$tableName);
+            ((fID=$i-1))
+            if [[ $colKey == "PK" ]]
+            then
+                break;
+            fi
+        done
+        val=$(zenity --entry --title="PK $colName" --text="Enter Value");
+        if [[ $val != "" ]]
+        then
+            res=$(awk 'BEGIN{FS=":"}{if ($'$fID'=="'$val'") print $'$fID'}' $tableName)
+            if [[ $res != "" ]]
+            then
+                NR=$(awk 'BEGIN{FS=":"}{if ($'$fID'=="'$val'") print NR}' $tableName)
+                sed -i ''$NR'd' $tableName 2>> /dev/null
+                if [[ $? == 0 ]]
+                then
+                    zenity --info --title="Info Message" --text="Row Deleted Successfully";
+                else
+                    zenity --error --title="Error Message" --text="Error Deleting Data From Table $tableName";
+                fi
+            else
+                zenity --error --title="Error Message" --text="Value not Found"
+            fi
+            tableMenu;
         fi
     fi
     tableMenu;
